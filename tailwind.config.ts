73d3f3f5a19184f2846b1a93c5ddf6512c539b6b
AETHERIUM469 <sukesh.gpillayphysio@gmail.com>
2025-06-07 14:01:40 +0200

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{tsx,ts,jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
